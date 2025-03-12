7d7bf21ae7c0b4d801d84a2b5f52e377c17e107d
Eszter, Gábor <mrxaxen@gmail.com>
2025-03-12 18:41:54 +0100

    First test version

--- ras_spt_server_interface/main.py
+++ ras_spt_server_interface/main.py
@@ -1,37 +1,46 @@
 # TODO: Only allow request handling from the bot as origin(based on ip/domain)
 import subprocess
 import os
+import uvicorn
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SPT_SERVER_SERVICE_NAME = os.getenv('SPT_SERVER_SERVICE_NAME')
+RAS_SPT_SERVER_SERVICE_NAME = os.getenv('RAS_SPT_SERVER_SERVICE_NAME')
+RAS_SPT_SERVER_HOST = os.getenv('RAS_SPT_SERVER_HOST')
+RAS_SPT_SERVER_PORT = os.getenv('RAS_SPT_SERVER_PORT')
+
+CMD_SERVER_OPS = {
+    'spt-server-start': 'start',
+    'spt-server-stop': 'stop',
+    'spt-server-restart': 'restart',
+}
+
 app = FastAPI()
 
 
-def check_service_name_defined():
-    if SPT_SERVER_SERVICE_NAME is None:
-        raise Exception('Service name not defined.')
+@app.post('/spt_server/{server_op_param}')
+async def server_start(server_op_param: str):
+    if RAS_SPT_SERVER_SERVICE_NAME is None:
+        raise HTTPException(status_code=503, detail='Service name not defined.')
+
+    try:
+        server_op = CMD_SERVER_OPS[server_op_param]
+        cmd = f'nssm {server_op} {RAS_SPT_SERVER_SERVICE_NAME}'.split()
+        process = subprocess.run(cmd)
+    except KeyError:
+        # TODO: LOG
+        print(f'No such operation found for the spt-server service: {server_op_param}')
 
 
-@app.post('/server/start')
-async def server_start(req: Request):
-    check_service_name_defined()
-    cmd = f'nssm start {SPT_SERVER_SERVICE_NAME}'.split()
+def main():
+    if RAS_SPT_SERVER_HOST is None or RAS_SPT_SERVER_PORT is None:
+        raise Exception('Server host or port is not defined! Exiting.')
 
-    process = subprocess.run(cmd)  # TODO: Either do an async await somehow, or use POpen
-
-
-@app.post('/server/stop')
-async def server_stop(req: Request):
-    check_service_name_defined()
-    cmd = f'nssm stop {SPT_SERVER_SERVICE_NAME}'.split()
-    process = subprocess.run(cmd)
+    uvicorn.run(app=app, host=RAS_SPT_SERVER_HOST, port=int(RAS_SPT_SERVER_PORT))
 
 
-@app.post('/server/restart')
-async def server_restart(req: Request):
-    check_service_name_defined()
-    pass
+if __name__ == '__main__':
+    main()
