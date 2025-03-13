dea3825894d2c108c461f609674c1668007294c9
Eszter, Gábor <mrxaxen@gmail.com>
2025-03-13 19:06:23 +0100

    Maybe quotes are interpreted weirdly?

--- ras_spt_server_interface/main.py
+++ ras_spt_server_interface/main.py
@@ -28,7 +28,7 @@ async def server_start(server_op_param: str):
 
     try:
         server_op = CMD_SERVER_OPS[server_op_param]
-        cmd = f'nssm {server_op} "{RAS_SPT_SERVER_SERVICE_NAME}"'.split()
+        cmd = f'nssm {server_op} {RAS_SPT_SERVER_SERVICE_NAME}'.split()
         print(f'Command to execute: {cmd}')
         process = subprocess.run(cmd, capture_output=True)
         print(f'Service returned: {process.stdout.decode()}\n{process.stderr.decode()}')
